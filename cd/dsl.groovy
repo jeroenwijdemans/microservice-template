bad93b00468e4c690cb8e93be321c11510f466cb
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2016-10-02 23:28:07 +0200

    escape all bash variable in jobdsl

--- cd/dsl.groovy
+++ cd/dsl.groovy
@@ -11,13 +11,13 @@ job("REST_TEMPLATE") {
     steps {
         shell("./gradlew clean fatJar copyDockerResources")
         shell("""
-              DIR="\$( cd "\$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+              DIR="\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
 
               NAMESPACE="test"
               NAME="template"
 
               VERSION=`./gradlew -q version`
-              DOCKER_IMAGE="\$JENKINS_DOCKER_REGISTRY}/\${NAMESPACE}/\${NAME}:\${VERSION}"
+              DOCKER_IMAGE="\$JENKINS_DOCKER_REGISTRY/\${NAMESPACE}/\${NAME}:\${VERSION}"
 
               cd ./build/libs
               docker build -t \${DOCKER_IMAGE} .
@@ -28,10 +28,10 @@ job("REST_TEMPLATE") {
                   echo "Build succesfully - pushing to registry"
                   docker push \${DOCKER_IMAGE}
 
-                   cd $DIR
+                   cd \$DIR
                    echo 'Start deployment ... '
 
-                   sed "s~{{DOCKER_IMAGE}}~${DOCKER_IMAGE}~"  ./cd/rc.yaml.template > ./cd/rc.yaml
+                   sed "s~{{DOCKER_IMAGE}}~\${DOCKER_IMAGE}~"  ./cd/rc.yaml.template > ./cd/rc.yaml
 
                    kubectl create -f ./cd/rc.yaml
                    kubectl create -f ./cd/service.yaml
